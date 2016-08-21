d6d37af21723f77557025bea3c9a3085fda4800e
nik strozak <nik.strozak@outlook.com>
2016-08-22 01:21:47 +0200

    treba še nagruntat tole knjižnico

--- ChromaImg/Form1.cs
+++ ChromaImg/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -42,59 +43,93 @@ namespace ChromaImg
 
         }
 
-        private int[,] colorArray= new int[6,15];
-        
-       
+        private double w;
+        private double h;
         public void popArrayColors(Bitmap map)
         {
 
-            double h = map.Height/6;
-            double w = map.Width/15;
+            h = map.Height/6;
+            w = map.Width/15;
+            redArrayA = new int[Convert.ToInt32(h)];
+            greenArrayA = new int[Convert.ToInt32(h)];
+            blueArrayA = new int[Convert.ToInt32(h)];
+            avgRGBOveral = new int[6,15,3];
+
             for (int i = 0; i < 15; i++)
             {
-                getColorAvg(h+h, w+w, map);
+                getColorAvg(h,w,map);
             }
 
 
         }
 
-        private void getColorAvg(double h, double w, Bitmap map)
+        private int[] redArrayA;
+        private int[] greenArrayA;
+        private int[] blueArrayA;
+        private int st = 0;
+        private void getRGBAvg(int[] r, int[] g, int[] b)
         {
-            int[] pixelArray = new int[Convert.ToInt32(h)];
-            int[] keyArray = new int[Convert.ToInt32(w)];
-            int c = 0;
-            int rez = 0;
+            //redArrayA = new int[Convert.ToInt32(h)];
+            //greenArrayA = new int[Convert.ToInt32(h)];
+            //blueArrayA = new int[Convert.ToInt32(h)];
+            int rezR=0, rezG=0, rezB=0;
             for (int i = 0; i < w; i++)
             {
-              
-                var a = 0;
-                for (int j = 0; j < h; j++)
-                {
-                    
-                    Color clr = map.GetPixel(i, j);
-                    int red = clr.R;
-                    int green = clr.G;
-                    int blue = clr.B;
-                    int avg = (red + green + blue) / 3;
-                    pixelArray[a] = avg;
-                    a++;
-                }
-                for (int j = 0; j < pixelArray.Length; j++)
-                {
-                    rez += pixelArray[j];
-                }
-
-                float avgP = rez / pixelArray.Length;
-                rez = 0;
-                keyArray[c] = Convert.ToInt32(avgP);
-                c++;
+                rezR += r[i];
+                rezG += g[i];
+                rezB += b[i];
             }
-            int avgBlock = 0;
-            for (int i = 0; i < keyArray.Length; i++)
+            redArrayA[st] = rezR/Convert.ToInt32(w);
+            greenArrayA[st] = rezG/Convert.ToInt32(w);
+            blueArrayA[st] = rezB/Convert.ToInt32(w);
+            st++;
+        }
+        private void getColorAvg(double h, double w, Bitmap map)
+        {
+            int[] redArray = new int[Convert.ToInt32(w)];
+            int[] greenArray = new int[Convert.ToInt32(w)];
+            int[] blueArray = new int[Convert.ToInt32(w)];
+            
+            for (int i = 0; i < h; i++)
             {
-                avgBlock += keyArray[i];
+                for (int j = 0; j < w; j++)
+                {
+                    Color clr = map.GetPixel(i, j);
+                    redArray[j] = clr.R;
+                    greenArray[j] = clr.G;
+                    blueArray[j] = clr.B;
+
+                }
+                getRGBAvg(redArray,greenArray,blueArray);
+
             }
-            //Keyboard.Instance.SetKeys();
+            st = 0;
+            calcAvgBlock(redArrayA,greenArrayA,blueArrayA);
+        }
+
+        private int[,,] avgRGBOveral;
+        private int visina = 0, sirina = 0, z = 0;
+        private void calcAvgBlock(int[] r, int[] g, int[] b)
+        {
+            //r=new int[Convert.ToInt32(w)];
+            //g=new int[Convert.ToInt32(w)];
+            //b=new int[Convert.ToInt32(w)];
+            int rezR = 0, rezG = 0, rezB = 0;
+            for (int i = 0; i < w; i++)
+            {
+                rezR += r[i];
+                rezG += g[i];
+                rezB += b[i];
+            }
+            // 3D array's https://msdn.microsoft.com/sl-si/library/2yd9wwz4.aspx
+            avgRGBOveral[visina, sirina, 0] = rezR/Convert.ToInt32(h);
+            avgRGBOveral[visina, sirina, 1] = rezG/Convert.ToInt32(h);
+            avgRGBOveral[visina, sirina, 2] = rezB/Convert.ToInt32(h);
+            int rdeca = avgRGBOveral[visina, sirina, 0];
+            //Color barva = new Color() { A = 255, R = rdeca, G = avgRGBOveral[visina, sirina, 1], B = avgRGBOveral[visina, sirina, 2] };
+
+            Keyboard.Instance.SetKeys(Corale.Colore.Core.Color.FromRgb(Convert.ToUInt32(avgRGBOveral[visina, sirina, 0]+ avgRGBOveral[visina, sirina, 1]+ avgRGBOveral[visina, sirina, 2])), Key.Macro5);
+
         }
         
         
